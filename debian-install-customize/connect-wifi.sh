74379921966a80e9c6eab5df136f1bb0d4343147
Alberto Romero <aerm1983@gmail.com>
2021-08-23 14:53:20 -0400

    script connect-wifi.sh adjusted

--- debian-install-customize/connect-wifi.sh
+++ debian-install-customize/connect-wifi.sh
@@ -5,39 +5,31 @@ w="wlp4s0"
 
 function __helper_fn { echo $comm ; sleep 1 ; eval $comm ; echo ; sleep 1 ; }
 
+comm="service avahi-daemon stop"
+__helper_fn
+
 
 comm="ip link set $w up"
-
 __helper_fn
 
 
-
-comm="ip link"
-
+comm="ip link | grep $w"
 __helper_fn
 
 
-
-comm="iwlist $w scan | grep -i essid"
-
+comm="iwlist $w scan | grep -E -i '(essid|busy|result)'"
 __helper_fn
-
 
 
 comm="wpa_supplicant -B -i $w -c /etc/wpa_supplicant/wpa_supplicant.conf"
-
 __helper_fn
-
 
 
 comm="iwconfig $w" 
-
 __helper_fn
 
 
-
-comm="dhclient $w"
-
+comm="dhclient -v $w"
 __helper_fn
 
 
