93a73ddfbaffbeb7bd1f5dbd00e41ca6b8514ac4
Alberto Romero <alberto.romero.eng@gmail.com>
2023-02-18 06:57:08 -0400

    [springboot] Add swagger-demo

---
+++ swagger-demo/src/main/java/localhost/SwaggerDemo/vehicle/VehiclePojo.java
@@ -0,0 +1,13 @@
+package localhost.SwaggerDemo.vehicle;
+
+public class VehiclePojo {
+	
+	public String name;
+	public Integer year;
+	
+	public VehiclePojo (String name, Integer year) {
+		this.name = name;
+		this.year = year;
+	}
+	
+}
