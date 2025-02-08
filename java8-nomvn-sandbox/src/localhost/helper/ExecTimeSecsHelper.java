806e1ef57b5a915a2767e769f858969c31005558
Alberto Romero <alberto.romero.eng@gmail.com>
2025-02-08 16:00:42 -0400

    [java] java8-nomvn, enhance ExecTimeSecsHelper

--- java8-nomvn-sandbox/src/localhost/helper/ExecTimeSecsHelper.java
+++ java8-nomvn-sandbox/src/localhost/helper/ExecTimeSecsHelper.java
@@ -1,16 +1,27 @@
 package localhost.helper;
 
 /**
- * Simple execution time calculator, in seconds.
+ * <p>Simple execution time calculator, in seconds.
+ * 
+ * <p>Update(s):
+ * <ul>
+ * <li> 2024-12-09 added attribute endTimeMs, method getLast(), class ETSH.
+ * </ul>
+ *  
  * 
  * @author Alberto Romero
  * @since 2024-03-12
  *
  */
-public class ExecTimeSecsHelper {
+public abstract class ExecTimeSecsHelper {
 
-	private long initTimeMs;
+	protected long initTimeMs;
+	protected long endTimeMs;
 
+
+	/**
+	 * Tests
+	 */
 	public static void main() {
 		System.out.println("Hello from ExecTimeHelper.main()!");
 
@@ -22,11 +33,11 @@ public class ExecTimeSecsHelper {
 			System.err.println("sleep, error: " + e);
 		}
 		long et00 = System.currentTimeMillis();
-		float ets00 = ExecTimeSecsHelper.getFrom(it00, et00);
+		float ets00 = ETSH.getFrom(it00, et00);
 		System.out.println("test00, static getFrom() with initTime, endTime --  ets00: " + ets00);
 
 		// test 01, instance with no argument
-		ExecTimeSecsHelper eth01 = ExecTimeSecsHelper.getInstance();
+		ETSH eth01 = ETSH.init();
 		try {
 			Thread.sleep(1500L);
 		} catch (Exception e) {
@@ -36,7 +47,7 @@ public class ExecTimeSecsHelper {
 		System.out.println("test01, instance with no argument, get() --  ets01: " + ets01);
 
 		// test 02, instance with argument
-		ExecTimeSecsHelper eth02 = ExecTimeSecsHelper.getInstance(System.currentTimeMillis());
+		ETSH eth02 = ETSH.init(System.currentTimeMillis());
 		try {
 			Thread.sleep(2000L);
 		} catch (Exception e) {
@@ -46,23 +57,136 @@ public class ExecTimeSecsHelper {
 		System.out.println("test01, instance with argument, get() --  ets02: " + ets02);
 	}
 
+
+	/*
+	 * Static methods
+	 */
+
+	/**
+	 * <p> Method kept for backwards compatibility.
+	 * <p> Possibly to be @Deprecated in the future.
+	 */
+	// @Deprecated
 	public static float getFrom(long initTimeMs, long endTimeMs) {
+		return ExecTimeSecsHelper.calculate(initTimeMs, endTimeMs);
+	}
+
+	/**
+	 * <p> Method created due to possible change to @Deprecated in 
+	 * {@link #getFrom(long, long)}.
+	 */
+	private static float calculate(long initTimeMs, long endTimeMs) {
 		return ( (float) (endTimeMs - initTimeMs) ) / 1000.0f;
 	}
 
-	public static ExecTimeSecsHelper getInstance() {
-		ExecTimeSecsHelper etsh = new ExecTimeSecsHelper();
-		etsh.initTimeMs = System.currentTimeMillis();
-		return etsh;
+	/**
+	 * <p> Get instance, initialize time using current instant. 
+	 */
+	public static <T extends ExecTimeSecsHelper> T getInstance(Class<T> clazz) {
+		T instance = null;
+		try {
+			instance = clazz.getConstructor().newInstance();
+		} catch (Throwable ex) {
+			instance = null;
+		}
+		return instance;
 	}
 
-	public static ExecTimeSecsHelper getInstance(long initTimeMs) {
-		ExecTimeSecsHelper etsh = new ExecTimeSecsHelper();
-		etsh.initTimeMs = initTimeMs;
-		return etsh;
+	/**
+	 * <p>Get instance, initialize using unix-time provided as parameter (millisecs). 
+	 */
+	public static <T extends ExecTimeSecsHelper> T getInstance(long initTimeMs, Class<T> clazz) {
+		T instance = null;
+		try {
+			instance = clazz.getConstructor().newInstance();
+			instance.initTimeMs = initTimeMs;
+			instance.endTimeMs = initTimeMs;
+		} catch (Throwable ex) {
+			instance = null;
+		}
+		return instance;
 	}
 
+
+	/*
+	 * Constructors
+	 */
+
+	public ExecTimeSecsHelper() {
+		this.initTimeMs = System.currentTimeMillis();
+		this.endTimeMs = this.initTimeMs;
+	}
+
+	public ExecTimeSecsHelper(long initTimeMs) {
+		this.initTimeMs = initTimeMs;
+		this.endTimeMs = this.initTimeMs;
+	}
+
+
+	/*
+	 * Time difference calculating methods
+	 */
+
+	/**
+	 * Get time elapsed to current instant.
+	 */
 	public float get() {
-		return getFrom(this.initTimeMs, System.currentTimeMillis());
+		this.endTimeMs = System.currentTimeMillis();
+		return ExecTimeSecsHelper.calculate(initTimeMs, endTimeMs);
+	}
+
+	/**
+	 * Previous time elapsed using get().
+	 */
+	public float getPrevious() {
+		return ExecTimeSecsHelper.calculate(initTimeMs, endTimeMs);
+	}
+
+
+
+	/**
+	 * <p>Helping class to aid aliasing on {@link localhost.helper.ExecTimeSecsHelper ExecTimeSecsHelper}.
+	 * 
+	 * <p>Methods of interest:
+	 * <ul>
+	 * <li> {@link #from(long, long)}
+	 * <li> {@link #init()}, {@link #init(long)}
+	 * <li> {@link #get()}
+	 * <li> {@link #last()}
+	 * </ul>
+	 * 
+	 * @author Alberto Romero
+	 * @since 2024-12-09
+	 */
+	public static class ETSH extends ExecTimeSecsHelper {
+
+		/**
+		 * <p> Alias for {@link localhost.helper.ExecTimeSecsHelper#getFrom(long, long) getFrom(long initTimeMs, long endTimeMs)}.
+		 */
+		public static float from(long initTimeMs, long endTimeMs) {
+			return ETSH.getFrom(initTimeMs, endTimeMs);
+		}
+
+		/**
+		 * <p> Alias for {@link localhost.helper.ExecTimeSecsHelper#getInstance(Class) getInstance(Class&lt;T&gt; clazz)}.
+		 */
+		public static ETSH init() {
+			return ETSH.getInstance(ETSH.class);
+		}
+
+		/**
+		 * <p> Alias for {@link localhost.helper.ExecTimeSecsHelper#getInstance(long, Class) getInstance(long initTimeMs, Class&lt;T&gt; clazz)}.
+		 */
+		public static ETSH init(long initTimeMs) {
+			return ETSH.getInstance(initTimeMs, ETSH.class);
+		}
+
+		/**
+		 * <p> Alias for {@link localhost.helper.ExecTimeSecsHelper#getPrevious() getPrevious()}.
+		 */
+		public float last() {
+			return this.getPrevious();
+		}
+
 	}
 }
